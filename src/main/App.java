6f746cfe5915d6ccb78d9cf084297ad9f5cfdca0
HassanMadkour <hassan20elmsriy@gmail.com>
2023-12-21 00:12:13 +0200

    hassan create new changes

--- src/main/App.java
+++ src/main/App.java
@@ -11,7 +11,7 @@ public class App {
         try {
 
             // read file in list
-            File tokenFile = new File("parser/resources/newToken.text");
+            File tokenFile = new File("resources/newToken.text");
             Token.tokens = Token.convertTheFiletoList(tokenFile);
 
             // for (String[] array : Token.tokens) {
