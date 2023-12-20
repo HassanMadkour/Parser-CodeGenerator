d805b32e3475b33f123b52b943d5d5c8af0c4ffa
HassanMadkour <hassan20elmsriy@gmail.com>
2023-12-20 02:20:31 +0200

    Description of the changes

--- src/main/App.java
+++ src/main/App.java
@@ -7,6 +7,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         try {
+
             File tokenFile = new File("N:/4thYear/first-Term/Compiler/project/parser/resources/Tokens.text");
             Token.tokens = Token.convertTheFiletoList(tokenFile);
 
--- src/rules/IF.java
+++ src/rules/IF.java
@@ -6,30 +6,45 @@ public class IF {
 
     public static void ifStatment() {
         if (Token.getNextToken()[0] == "(") {
-            if(){
-                if(Token.getNextToken()[0] == ")"){
-                    switch (Token.getNextToken()[0] ) {
-                        case "{":{
+            if (true
+            // condition
+            ) {
+                if (Token.getNextToken()[0] == ")") {
+                    if (Token.getNextToken()[0] == "{") {
                         Statment.statment(Token.getNextToken());
                         if (Token.getNextToken()[0] == "}") {
-                                        System.out.println("NO ERROR");
+                            if (Token.getNextToken()[0] == "else") {
+                                if (Token.getNextToken()[0] == "{") {
+                                    Statment.statment(Token.getNextToken());
+                                    if (Token.getNextToken()[0] == "}") {
+                                        System.err.println("");
+                                    } else {
+                                        System.err.println(" } not exist  ");
+                                    }
+                                } else {
+                                    System.err.println(" prehaps this { not exist");
 
+                                }
+                            } // end of else
+
+                        } else {
+                            System.err.println("} not exist ");
                         }
+                    } else {
+                        System.err.println(" prehaps this { not exist");
                     }
-                            
-                            break;
-                    
-                        default:
-                            break;
-                    }
-                }else{
-            System.err.println("Error Syntax");
+
+                } else {
+                    System.err.println("Error Syntax )");
+                }
+            } else {
+                System.err.println("error in condtion");
+            }
+        } else
+
+        {
+            System.err.println("Error Syntax ( ");
         }
-            }else{
-            System.err.println("Error Syntax");
-        }
-        } else {
-            System.err.println("Error Syntax");
-        }
+
     }
 }
