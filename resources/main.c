5666e0185135bb3dd9a12d67059146c7bd37dfb5
Ahmed gomaa madkour <119963052+Ahmedmadkour640@users.noreply.github.com>
2023-12-25 01:44:05 +0200

    Update main.c

---
+++ resources/main.c
@@ -0,0 +1,21 @@
+int a = 10 ;
+double b = 11 ;
+if ( a < 12 ) {
+    a = b + 1 ;
+} else {
+    b = a - 1 ;
+}
+while ( x == 5 ) { x = y + 4 ; }
+for ( int x = 4 ; x < 6 ; x = r + 8 ) {
+                           a = b + 1 ; }
+        do { m = n + 9 ; } while ( x > 9 ) ;
+        n = m + 9 ;
+        switch ( w ) { case 6 :
+         n = m + 9 ;
+         break ;
+        case 2 :
+         n = m + 9 ;
+          break ;
+        default :
+        n = m + 9 ;
+        }
