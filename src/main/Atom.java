fe54ff04a6c19aa5c9fd23cc62d25c828e6cb829
HassanMadkour <hassan20elmsriy@gmail.com>
2023-12-28 23:23:48 +0200

    first commit

---
+++ src/main/Atom.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.List;
+
+class Atom {
+
+    static public int labelindex = 0;
+    static public List<String> atomList ;
+
+    public static String getNextLabel() {
+        return "L" + labelindex++;
+    }
+    public static String getCurrentLabel() {
+        return "L" + --labelindex;
+    }
+    public static String getPreviousLabel() {
+        return "L" + (labelindex-2);
+    }
+
+
+}
