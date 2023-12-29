fdb0e239bd0ae707b5e664c2ad132c1050d4f032
hosamazab <65652392+hosamazab@users.noreply.github.com>
2023-12-29 12:44:19 +0200

    Update IF.java

--- src/rules/IF.java
+++ src/rules/IF.java
@@ -1,18 +1,22 @@
 package rules;
-
+import main.Atom;
 import main.Token;
 
 public class IF {
 
     public static boolean ifStatment() {
-
         if (Token.getNextToken()[0].equals("(")) {
-
             if (ConditionParser.isCondition()) {
                 if (Token.getNextToken()[0].equals(")")) {
                     if (Token.getNextToken()[0].equals("{")) {
                         // write new statment
+                        Atom.atomList.add("(JMP,"+Atom.getNextLabel()+")");
+                       
+                        Atom.atomList.add("(LBL,"+Atom.getPreviousLabel()+")");
+                        
                         Statment.statment(Token.getNextToken());
+                        Atom.atomList.add("(LBL,"+Atom.getCurrentLabel()+")");
+                       
                         // to stop if error show
                         if (!Statment.vaildation()) {
                             return false;
