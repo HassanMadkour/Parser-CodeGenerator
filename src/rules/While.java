5b45871b77c120988ecc60e28dcecf031f8754b6
hosamazab <65652392+hosamazab@users.noreply.github.com>
2023-12-29 12:44:45 +0200

    Update While.java

--- src/rules/While.java
+++ src/rules/While.java
@@ -1,5 +1,6 @@
 package rules;
 
+import main.Atom;
 import main.Token;
 
 public class While {
@@ -7,10 +8,15 @@ public class While {
     public static boolean whilestatement() {
 
         if (Token.getNextToken()[0].equals("(")) {
+            Atom.atomList.add("(LBL,"+Atom.getNextLabel()+")");
             if (ConditionParser.isCondition()) {
+                Atom.atomList.add("(JMP,"+Atom.getNextLabel()+")");
+                Atom.atomList.add("(LBL,"+Atom.getPreviousLabel()+")");
                 if (Token.getNextToken()[0].equals(")")) {
                     if (Token.getNextToken()[0].equals("{")) {
                         Statment.statment(Token.getNextToken());
+                        Atom.atomList.add("(JMP,"+Atom.getPreviousLabelp2()+")");
+                        Atom.atomList.add("(LBL,"+Atom.getCurrentLabel()+")");  
                         if (!Statment.vaildation()) {
                             return false;
                         }
