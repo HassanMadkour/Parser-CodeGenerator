dade9bd172953dd2931453853425561737950056
hosamazab <65652392+hosamazab@users.noreply.github.com>
2023-12-29 12:43:47 +0200

    Update For.java

--- src/rules/For.java
+++ src/rules/For.java
@@ -1,17 +1,28 @@
 package rules;
 
+import main.Atom;
 import main.Token;
 
 public class For {
     public static boolean forStatment() {
         if (Token.getNextToken()[0].equals("(")) {
             if (Token.getNextToken()[1].equals("datatype") && VariableDefinitionParser.isValidVariableDefinition()) {
+                Atom.atomList.add("(LBL,"+Atom.getNextLabel()+")");
                 if (ConditionParser.isCondition()) {
+                    Atom.atomList.add("(JMP,"+Atom.getNextLabel()+")");
+                    Atom.atomList.add("(LBL,"+Atom.getPreviousLabel()+")");                  
                     if (Token.getNextToken()[0].equals(";")) {
                         if (Token.getNextToken()[1].equals("Identifier") && ExpressionParser.isValidExpression(true)) {
                             if (Token.getNextToken()[0].equals(")")) {
                                 if (Token.getNextToken()[0].equals("{")) {
                                     Statment.statment(Token.getNextToken());
+                                    String a1 = Atom.atomList.get(Atom.atomList.size()-1);
+                                    String a2 = Atom.atomList.get(Atom.atomList.size()-2);
+                                    Atom.atomList.set(Atom.atomList.size()-1, a2);
+                                    Atom.atomList.set(Atom.atomList.size()-1, a1);
+                                    Atom.atomList.add("(JMP,"+Atom.getPreviousLabelp2()+")");
+                                    Atom.atomList.add("(LBL,"+Atom.getCurrentLabel()+")");   
+                                    System.err.println(Atom.atomList);
                                     if (!Statment.vaildation()) {
                                         return false;
                                     }
