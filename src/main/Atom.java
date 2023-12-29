0c75dd6a73b54b83cdb3ffa24b3cebbae9e94168
hosamazab <65652392+hosamazab@users.noreply.github.com>
2023-12-29 12:43:10 +0200

    Update Atom.java

--- src/main/Atom.java
+++ src/main/Atom.java
@@ -1,23 +1,26 @@
 package main;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Atom {
 
     static public int labelindex = 0;
-    static public List<String> atomList= new ArrayList<>();;
+    static public List<String> atomList = new ArrayList<>();
 
     public static String getNextLabel() {
-        return "L" + ++labelindex;
+        return "L" + labelindex++;
     }
 
     public static String getCurrentLabel() {
-        return "L" + --labelindex;
+        return "L" + (labelindex - 1);
     }
 
     public static String getPreviousLabel() {
         return "L" + (labelindex - 2);
+    }
+    public static String getPreviousLabelp2() {
+        return "L" + (labelindex - 3);
     }
 
 }
